0c9d1bb3715696c0fee5a27d34a7bf79c855998a
root <root@localhost.localdomain>
2017-10-09 22:16:32 +1300

    Update

---
+++ crsp/crsp_projects/doctype/cctf_project/test_cctf_project.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017, Dennis Tatila and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+class TestCCTFProject(unittest.TestCase):
+	pass
---
+++ crsp/crsp_projects/doctype/district_id/district_id.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Dennis Tatila and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('District ID', {
+	refresh: function(frm) {
+
+	}
+});
